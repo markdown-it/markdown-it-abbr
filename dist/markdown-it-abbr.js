676d08a99e4baecdbf01e2a9d5a0599a32f08cc5
Vitaly Puzrin <vitaly@rcdesign.ru>
2015-12-31 18:08:52 +0300

    Browser files rebuild

--- dist/markdown-it-abbr.js
+++ dist/markdown-it-abbr.js
@@ -1,10 +1,20 @@
-/*! markdown-it-abbr 1.0.0 https://github.com//markdown-it/markdown-it-abbr @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitAbbr = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/*! markdown-it-abbr 1.0.2 https://github.com//markdown-it/markdown-it-abbr @license MIT */(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.markdownitAbbr = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports=/[!-#%-\*,-/:;\?@\[-\]_\{\}\xA1\xA7\xAB\xB6\xB7\xBB\xBF\u037E\u0387\u055A-\u055F\u0589\u058A\u05BE\u05C0\u05C3\u05C6\u05F3\u05F4\u0609\u060A\u060C\u060D\u061B\u061E\u061F\u066A-\u066D\u06D4\u0700-\u070D\u07F7-\u07F9\u0830-\u083E\u085E\u0964\u0965\u0970\u0AF0\u0DF4\u0E4F\u0E5A\u0E5B\u0F04-\u0F12\u0F14\u0F3A-\u0F3D\u0F85\u0FD0-\u0FD4\u0FD9\u0FDA\u104A-\u104F\u10FB\u1360-\u1368\u1400\u166D\u166E\u169B\u169C\u16EB-\u16ED\u1735\u1736\u17D4-\u17D6\u17D8-\u17DA\u1800-\u180A\u1944\u1945\u1A1E\u1A1F\u1AA0-\u1AA6\u1AA8-\u1AAD\u1B5A-\u1B60\u1BFC-\u1BFF\u1C3B-\u1C3F\u1C7E\u1C7F\u1CC0-\u1CC7\u1CD3\u2010-\u2027\u2030-\u2043\u2045-\u2051\u2053-\u205E\u207D\u207E\u208D\u208E\u2308-\u230B\u2329\u232A\u2768-\u2775\u27C5\u27C6\u27E6-\u27EF\u2983-\u2998\u29D8-\u29DB\u29FC\u29FD\u2CF9-\u2CFC\u2CFE\u2CFF\u2D70\u2E00-\u2E2E\u2E30-\u2E42\u3001-\u3003\u3008-\u3011\u3014-\u301F\u3030\u303D\u30A0\u30FB\uA4FE\uA4FF\uA60D-\uA60F\uA673\uA67E\uA6F2-\uA6F7\uA874-\uA877\uA8CE\uA8CF\uA8F8-\uA8FA\uA92E\uA92F\uA95F\uA9C1-\uA9CD\uA9DE\uA9DF\uAA5C-\uAA5F\uAADE\uAADF\uAAF0\uAAF1\uABEB\uFD3E\uFD3F\uFE10-\uFE19\uFE30-\uFE52\uFE54-\uFE61\uFE63\uFE68\uFE6A\uFE6B\uFF01-\uFF03\uFF05-\uFF0A\uFF0C-\uFF0F\uFF1A\uFF1B\uFF1F\uFF20\uFF3B-\uFF3D\uFF3F\uFF5B\uFF5D\uFF5F-\uFF65]|\uD800[\uDD00-\uDD02\uDF9F\uDFD0]|\uD801\uDD6F|\uD802[\uDC57\uDD1F\uDD3F\uDE50-\uDE58\uDE7F\uDEF0-\uDEF6\uDF39-\uDF3F\uDF99-\uDF9C]|\uD804[\uDC47-\uDC4D\uDCBB\uDCBC\uDCBE-\uDCC1\uDD40-\uDD43\uDD74\uDD75\uDDC5-\uDDC8\uDDCD\uDE38-\uDE3D]|\uD805[\uDCC6\uDDC1-\uDDC9\uDE41-\uDE43]|\uD809[\uDC70-\uDC74]|\uD81A[\uDE6E\uDE6F\uDEF5\uDF37-\uDF3B\uDF44]|\uD82F\uDC9F/
+},{}],2:[function(require,module,exports){
+module.exports=/[ \xA0\u1680\u2000-\u200A\u2028\u2029\u202F\u205F\u3000]/
+},{}],3:[function(require,module,exports){
 // Enclose abbreviations in <abbr> tags
 //
 'use strict';
 
 
-var PUNCT_CHARS = ' \n()[]\'".,!?-';
+// ASCII characters in Cc, Sc, Sm, Sk categories we should terminate on;
+// you can check character classes here:
+// http://www.unicode.org/Public/UNIDATA/UnicodeData.txt
+var OTHER_CHARS      = ' \r\n$+<=>^`|~';
+
+var UNICODE_PUNCT_RE = require('uc.micro/categories/P/regex').source;
+var UNICODE_SPACE_RE = require('uc.micro/categories/Z/regex').source;
 
 
 module.exports = function sub_plugin(md) {
@@ -57,22 +67,31 @@ module.exports = function sub_plugin(md) {
 
 
   function abbr_replace(state) {
-    var i, j, l, tokens, token, text, nodes, pos, reg, m, regText,
+    var i, j, l, tokens, token, text, nodes, pos, reg, m, regText, regSimple,
         currentToken,
         blockTokens = state.tokens;
 
     if (!state.env.abbreviations) { return; }
-    if (!state.env.abbrRegExp) {
-      regText = '(^|[' + PUNCT_CHARS.split('').map(escapeRE).join('') + '])'
-              + '(' + Object.keys(state.env.abbreviations).map(function (x) {
-                        return x.substr(1);
-                      }).sort(function (a, b) {
-                        return b.length - a.length;
-                      }).map(escapeRE).join('|') + ')'
-              + '($|[' + PUNCT_CHARS.split('').map(escapeRE).join('') + '])';
-      state.env.abbrRegExp = new RegExp(regText, 'g');
-    }
-    reg = state.env.abbrRegExp;
+
+    regSimple = new RegExp('(?:' +
+      Object.keys(state.env.abbreviations).map(function (x) {
+        return x.substr(1);
+      }).sort(function (a, b) {
+        return b.length - a.length;
+      }).map(escapeRE).join('|') +
+    ')');
+
+    regText = '(^|' + UNICODE_PUNCT_RE + '|' + UNICODE_SPACE_RE +
+                    '|[' + OTHER_CHARS.split('').map(escapeRE).join('') + '])'
+            + '(' + Object.keys(state.env.abbreviations).map(function (x) {
+                      return x.substr(1);
+                    }).sort(function (a, b) {
+                      return b.length - a.length;
+                    }).map(escapeRE).join('|') + ')'
+            + '($|' + UNICODE_PUNCT_RE + '|' + UNICODE_SPACE_RE +
+                    '|[' + OTHER_CHARS.split('').map(escapeRE).join('') + '])';
+
+    reg = new RegExp(regText, 'g');
 
     for (j = 0, l = blockTokens.length; j < l; j++) {
       if (blockTokens[j].type !== 'inline') { continue; }
@@ -88,8 +107,12 @@ module.exports = function sub_plugin(md) {
         reg.lastIndex = 0;
         nodes = [];
 
+        // fast regexp run to determine whether there are any abbreviated words
+        // in the current token
+        if (!regSimple.test(text)) { continue; }
+
         while ((m = reg.exec(text))) {
-          if (reg.lastIndex > pos) {
+          if (m.index > 0 || m[1].length > 0) {
             token         = new state.Token('text', '', 0);
             token.content = text.slice(pos, m.index + m[1].length);
             nodes.push(token);
@@ -106,7 +129,8 @@ module.exports = function sub_plugin(md) {
           token         = new state.Token('abbr_close', 'abbr', -1);
           nodes.push(token);
 
-          pos = reg.lastIndex - m[3].length;
+          reg.lastIndex -= m[3].length;
+          pos = reg.lastIndex;
         }
 
         if (!nodes.length) { continue; }
@@ -124,8 +148,9 @@ module.exports = function sub_plugin(md) {
   }
 
   md.block.ruler.before('reference', 'abbr_def', abbr_def, { alt: [ 'paragraph', 'reference' ] });
-  md.core.ruler.after('inline', 'abbr_replace', abbr_replace);
+
+  md.core.ruler.after('linkify', 'abbr_replace', abbr_replace);
 };
 
-},{}]},{},[1])(1)
+},{"uc.micro/categories/P/regex":1,"uc.micro/categories/Z/regex":2}]},{},[3])(3)
 });
